cb9558eda4ce6bb1853f27604c061846e04063a8
Abhishek Sapkale <85690531+Abhi96Sapkale@users.noreply.github.com>
2021-08-18 10:29:20 +0530

    String Reverse

---
+++ strReverse.js
@@ -0,0 +1,14 @@
+const str = 'hello I am Abhishek';
+
+/*const newStr = str
+    .split('')
+    .reverse()
+    .join('');
+*/
+let newStr = '';
+for(let i=str.length - 1;i>=0;i--)
+{
+    newStr += str[i];
+}
+console.log(newStr);
+
